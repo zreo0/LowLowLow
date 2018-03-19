14341ad88fc9eefa83369c28449ea2c6dd0d6df8
zreo0 <zhengbanzw@hotmail.com>
2018-03-19 12:35:35 +0800

    add

---
+++ src/LayaSample.js
@@ -0,0 +1,10 @@
+//laya初始化
+Laya.init(480, 852, Laya.WebGL);
+//FPS
+Laya.Stat.show(0,0);
+//设置适配模式
+Laya.stage.scaleMode = 'exactfit';
+//设置居中对齐
+Laya.stage.alignH = 'center';
+//设置横屏
+Laya.stage.screenMode = 'vertical';
---
+++ src/runGame/RunGame.js
@@ -0,0 +1,5 @@
+(function () {
+    function RunGame(){
+        
+    }
+})();
