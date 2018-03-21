67fe355b4142c87c28772cbf4b415be8bdf31167
zreo0 <zhengbanzw@hotmail.com>
2018-03-21 16:34:57 +0800

    加入聊天功能

--- src/LayaSample.js
+++ src/LayaSample.js
@@ -1,10 +1,9 @@
 //laya初始化
-Laya.init(480, 852, Laya.WebGL);
+var layaCanvas = Laya.init(320,568,Laya.WebGL);
+layaCanvas.style.position  = 'relative';
 //FPS
 Laya.Stat.show(0,0);
-//设置适配模式
-Laya.stage.scaleMode = 'exactfit';
-//设置居中对齐
-Laya.stage.alignH = 'center';
-//设置横屏
-Laya.stage.screenMode = 'vertical';
+// //设置适配模式
+// Laya.stage.scaleMode = 'exactfit';
+// //设置居中对齐
+// Laya.stage.alignH = 'center';
